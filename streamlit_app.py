375a64a339c21f37d281799bad1ae7d9db8eac37
nabilaa16 <bilabili2026@gmail.com>
2025-05-27 09:12:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 nabilagemoy ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.imange("IMG_20250527_084454.jpg, width=200)
